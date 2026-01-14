1897fdf7ef08628ed2daa1d0e61415afb3f36a8e
markvrepo <78107955+markvrepo@users.noreply.github.com>
2026-01-13 21:56:52 -0500

    Update target files for Unreal Engine 5.5+ compatibility
    Added conditional logic in CavrnusCollabViewer.Target.cs and CavrnusCollabViewerEditor.Target.cs to set DefaultBuildSettings and IncludeOrderVersion based on Unreal Engine version. This ensures compatibility with both UE 5.5 or later and earlier versions.

--- Source/CavrnusCollabViewer.Target.cs
+++ Source/CavrnusCollabViewer.Target.cs
@@ -1,13 +1,20 @@
-// Copyright (c) 2024 Cavrnus. All rights reserved.
 using UnrealBuildTool;
 using System.Collections.Generic;
 
-public class CavrnusCollabViewerTarget  : TargetRules
+public class CavrnusCollabViewerTarget : TargetRules
 {
-	public CavrnusCollabViewerTarget (TargetInfo Target) : base(Target)
-	{
-		Type = TargetType.Game;
-		DefaultBuildSettings = BuildSettingsVersion.Latest;
-		ExtraModuleNames.AddRange( new string[] { "CavrnusCollabViewer" } );
-	}
+    public CavrnusCollabViewerTarget(TargetInfo Target) : base(Target)
+    {
+        Type = TargetType.Game;
+
+#if UE_5_5_OR_LATER
+        DefaultBuildSettings = BuildSettingsVersion.V6;
+        IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_7;
+#else
+        DefaultBuildSettings = BuildSettingsVersion.V5;
+        IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_0;
+#endif
+
+        ExtraModuleNames.AddRange(new string[] { "CavrnusCollabViewer" });
+    }
 }
--- Source/CavrnusCollabViewerEditor.Target.cs
+++ Source/CavrnusCollabViewerEditor.Target.cs
@@ -1,14 +1,20 @@
-// Copyright (c) 2024 Cavrnus. All rights reserved.
 using UnrealBuildTool;
 using System.Collections.Generic;
 
 public class CavrnusCollabViewerEditorTarget : TargetRules
 {
-	public CavrnusCollabViewerEditorTarget(TargetInfo Target) : base(Target)
-	{
-		Type = TargetType.Editor;
-		DefaultBuildSettings = BuildSettingsVersion.Latest;
+    public CavrnusCollabViewerEditorTarget(TargetInfo Target) : base(Target)
+    {
+        Type = TargetType.Editor;
 
-		ExtraModuleNames.AddRange( new string[] { "CavrnusCollabViewer" } );
-	}
+#if UE_5_5_OR_LATER
+        DefaultBuildSettings = BuildSettingsVersion.V6;
+        IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_7;
+#else
+        DefaultBuildSettings = BuildSettingsVersion.V5;
+        IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_0;
+#endif
+
+        ExtraModuleNames.AddRange(new string[] { "CavrnusCollabViewer" });
+    }
 }
